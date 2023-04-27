33fa4eeecca7e3bc30fa7cec16bdc31d82dc3bfd
aryormdhnn <aryoromadhon8@gmail.com>
2023-04-27 23:26:51 +0700

    update code

--- main.js
+++ main.js
@@ -1,112 +1,19 @@
-// let blogPosts = [
-//     {
-//       image: "asset/blog1.jpeg",
-//       title: "5 Cara Turunkan Kolesterol dengan Cepat, Ganti Daging dengan Ikan",
-//       text: "Di momen Idulfitri, menyantap menu khas Lebaran yang banyak mengandung santan dan lemak, dapat meningkatkan kolesterol tubuh jika dikonsumsi berlebihan."
-//     },
-//     {
-//       image: "asset/blog2.jpg",
-//       title: "5 Tips Agar Tak Batal Puasa di Tengah Cuaca Ekstrem",
-//       text: "Berpuasa di tengah cuaca panas yang cukup ekstrem terasa sangat menyiksa. Apalagi, akhir-akhir ini cuaca di Jakarta dan sekitarnya juga terasa semakin panas."
-//     },
-//     {
-//       image: "asset/image1.png",
-//       title: "Indonesia Diterjang Panas Mendidih, Ini Wanti-wanti Kemenkes Biar Tubuh Tetap Sehat",
-//       text: "Ketika marah, setiap orang dapat menunjukkan emosinya dengan cara yang berbeda-beda."
-//     },
-//     {
-//         image: "asset/image1.png",
-//         title: "13 Buah Pelancar ASI yang Bagus untuk Booster Alami",
-//         text: "Selama masa menyusui, ibu mungkin akan mengonsumsi banyak makanan yang"
-//       },
-//       {
-//         image: "asset/image1.png",
-//         title: "10 Cara Mengendalikan Emosi dengan Tepat saat Marah",
-//         text: "Ketika marah, setiap orang dapat menunjukkan emosinya dengan cara yang berbeda-beda."
-//       },
-//       {
-//         image: "asset/image1.png",
-//         title: "13 Buah Pelancar ASI yang Bagus untuk Booster Alami",
-//         text: "Selama masa menyusui, ibu mungkin akan mengonsumsi banyak makanan yang"
-//       },
-//       {
-//         image: "asset/image1.png",
-//         title: "10 Cara Mengendalikan Emosi dengan Tepat saat Marah",
-//         text: "Ketika marah, setiap orang dapat menunjukkan emosinya dengan cara yang berbeda-beda."
-//       },  {
-//         image: "asset/image1.png",
-//         title: "13 Buah Pelancar ASI yang Bagus untuk Booster Alami",
-//         text: "Selama masa menyusui, ibu mungkin akan mengonsumsi banyak makanan yang"
-//       },
-//       {
-//         image: "asset/image1.png",
-//         title: "10 Cara Mengendalikan Emosi dengan Tepat saat Marah",
-//         text: "Ketika marah, setiap orang dapat menunjukkan emosinya dengan cara yang berbeda-beda."
-//       },
-//     {
-//       image: "asset/image1.png",
-//       title: "13 Buah Pelancar ASI yang Bagus untuk Booster Alami",
-//       text: "Selama masa menyusui, ibu mungkin akan mengonsumsi banyak makanan yang"
-//     },
-//     {
-//       image: "asset/image1.png",
-//       title: "10 Cara Mengendalikan Emosi dengan Tepat saat Marah",
-//       text: "Ketika marah, setiap orang dapat menunjukkan emosinya dengan cara yang berbeda-beda."
-//     }
-//   ];
-
-//   // Loop untuk membuat kartu blog
-// const blogPostsContainer = document.getElementById("blog-posts");
-// const postsPerPage = 6; // jumlah kartu blog yang ditampilkan per halaman
-// let currentPage = 1; // halaman saat ini
-
-// // fungsi untuk menampilkan kartu blog pada halaman tertentu
-// function displayBlogPosts(posts, container, page) {
-//   const start = postsPerPage * (page - 1);
-//   const end = start + postsPerPage;
-//   const paginatedPosts = posts.slice(start, end);
-
-//   paginatedPosts.forEach(function (post) {
-//     const blogPost = document.createElement("div");
-//     blogPost.classList.add("col-md-4");
-//     blogPost.innerHTML = `
-//       <div class="card border-0">
-//         <img src="${post.image}" class="card-img-top" alt="${post.title}">
-//         <div class="card-body px-0">
-//           <h5 class="card-title">${post.title.length > 65 ? post.title.slice(0, 65) + '...' : post.title}</h5>
-//           <p class="card-text mt-3">${post.text.length > 80 ? post.text.slice(0, 80) + '...' : post.text}</p>
-//           <button class="btn btn-outline-primary">Lihat Semuanya</button>
-//         </div>
-//       </div>
-//     `;
-//     container.appendChild(blogPost);
-//   });
-// }
-
-
-// // menampilkan kartu blog pada halaman saat ini
-// displayBlogPosts(blogPosts, blogPostsContainer, currentPage);
-
-// // tambahkan event listener pada tombol "Load More" untuk memuat lebih banyak kartu blog
-// const loadMoreButton = document.getElementById("load-more-btn");
-// loadMoreButton.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   currentPage++;
-//   displayBlogPosts(blogPosts, blogPostsContainer, currentPage);
-// });
-
+// Mendefinisikan variabel untuk kontainer postingan blog dan tombol "Muat Lebih Banyak"
 const blogPostsContainer = document.getElementById("blog-posts");
 const loadMoreButton = document.getElementById("load-more-btn");
 
+// Mengatur halaman yang sedang aktif dan jumlah postingan blog yang ditampilkan pada setiap halaman
 let currentPage = 1;
 const postsPerPage = 6;
 
+// Fungsi untuk menampilkan postingan blog ke dalam kontainer yang ditentukan sesuai halaman yang diminta
 function displayBlogPosts(posts, container, page) {
   const start = postsPerPage * (page - 1);
   const end = start + postsPerPage;
   const paginatedPosts = posts.slice(start, end);
 
   paginatedPosts.forEach(function (post) {
+    // Membuat elemen HTML untuk setiap postingan blog
     const blogPost = document.createElement("div");
     blogPost.classList.add("col-md-4");
     blogPost.innerHTML = `
@@ -119,17 +26,21 @@ function displayBlogPosts(posts, container, page) {
         </div>
       </div>
     `;
+    // Menambahkan elemen HTML ke dalam kontainer postingan blog
     container.appendChild(blogPost);
   });
 }
 
+// Fungsi untuk memuat data postingan blog dari API menggunakan metode fetch()
 function fetchBlogPosts(page) {
   fetch(`https://6449942fb88a78a8f00b50c0.mockapi.io/blog-posts?page=${page}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      // Menampilkan data postingan blog di halaman web menggunakan fungsi displayBlogPosts()
       displayBlogPosts(data, blogPostsContainer, currentPage);
+      // Menyembunyikan tombol "Muat Lebih Banyak" jika data postingan blog sudah habis
       if (data.length < postsPerPage) {
         loadMoreButton.style.display = "none";
       }
@@ -139,10 +50,13 @@ function fetchBlogPosts(page) {
     });
 }
 
+// Memuat postingan blog yang pertama kali muncul di halaman web
 fetchBlogPosts(currentPage);
 
+// Menambahkan event listener pada tombol "Muat Lebih Banyak" untuk memuat postingan blog berikutnya
 loadMoreButton.addEventListener("click", function (event) {
   event.preventDefault();
+  // Menambahkan nilai currentPage dengan 1 agar halaman yang dimuat berikutnya adalah halaman berikutnya
   currentPage++;
   fetchBlogPosts(currentPage);
 });
